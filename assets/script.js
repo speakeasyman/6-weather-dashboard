424cbcb6826728bcb23a616c05e320f0e9fa08b1
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-05-09 14:06:13 -0400

    created cards for the 5day forecast

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var lon ='78.90';
 var weatherURL = 'api.openweathermap.org/data/2.5/weather?lat=';
 var searchBtn = $('#search');
 var testURL = 'http://api.openweathermap.org/data/2.5/onecall?lat=35.99&lon=-78.90&exclude=minutely,hourly&appid=f516944114208483a59c1cf67915c8cd'
-
+var dateTime = null;
 console.log(searchBtn)
 
 searchBtn[0].addEventListener('click', function(){
@@ -21,19 +21,25 @@ searchBtn[0].addEventListener('click', function(){
                     tempC = tempK - 273;
                     tempF = (tempC*1.8) + 32;
                     console.log('tempC', tempC);
+                    city = 'Uptopia';
+                    date = moment().format('MM/DD/YYYY');
+                    // icon = data.current.weater.icon;
+                    
+
+                    
                     
 
                     dailyWeather();
                 })
-            }
-        })
-});
+            
+        }
+})});
 
 
 function dailyWeather() {
     $('#temperature').text(tempF.toFixed(2));
     $('#wind').text(windSpeed);
     $('#humidity').text(humidity);
-    $('#uvIndex').text(uvIndex);
-
+    $('#uvIndex').text(uvIndex);    
+    $('#cityDate').text(city + '  '+date + ' ');
 }
