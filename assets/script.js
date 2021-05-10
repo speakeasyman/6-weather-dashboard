d5b5f6ed98bb544a69ec75c1b7d5655cb358b8f3
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-05-09 20:49:04 -0400

    making a back up before I try to clean up this code

--- assets/script.js
+++ assets/script.js
@@ -3,27 +3,22 @@ var lon ='';
 var weatherURL = 'api.openweathermap.org/data/2.5/weather?lat=';
 var searchBtn = $('#search');
 var dateTime = null;
-console.log(searchBtn)
 var temperatureF = '';
 var cityName = $('#cityName');
 var apiKey = '&appid=f516944114208483a59c1cf67915c8cd';
 var tempURL = 'http://api.openweathermap.org/data/2.5/onecall?lat=35.99&lon=-78.90&exclude=minutely,hourly&appid=f516944114208483a59c1cf67915c8cd'
-
+var historyBtn = $('.historyBtn');
 
 
 searchBtn[0].addEventListener('click', function(){
     city = cityName[0].value.trim()
-    console.log(city);
     var testURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial'+apiKey
     var temp2 = fetch(testURL)
     .then(function (response){
         if (response.ok) {
-            response.json().then(function (data2){                    
-                console.log(data2);
-                lat = data2.coord.lat
-                console.log(lat)
-                lon = data2.coord.lon
-                console.log(lon);
+            response.json().then(function (data2){                  
+            lat = data2.coord.lat
+            lon = data2.coord.lon
                 city = data2.name
                 storeSearch();
                 var latlonURL = 'http://api.openweathermap.org/data/2.5/onecall?lat=-'+lat+'&lon='+lon+'&exclude=minutely,hourly&appid=f516944114208483a59c1cf67915c8cd'
@@ -31,30 +26,24 @@ searchBtn[0].addEventListener('click', function(){
                 .then(function (response){
                     if (response.ok) {
                         response.json().then(function (data){
-                            console.log(`temp`, temp);
-                            console.log(`data`, data);
                             windSpeed = data.current.wind_speed + ' ';
                             humidity = data.current.humidity + ' ';
                             uvIndex = data.current.uvi;
                             tempK = data.current.temp;
                             tempC = tempK - 273;
-                            tempF = (tempC*1.8) + 32;
-                            console.log('tempC', tempC);                    
+                            tempF = (tempC*1.8) + 32;                    
                             date = moment().format('MM/DD/YYYY');
                             iconCurrent = data.current.weather[0].icon
                             var fiveDay = [];
                             for (let i = 1; i < 6; i++) {
                                 icon = data.daily[i].weather[0].icon;
                                 var iconURL = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
-                                        console.log('chosen icon',icon);
                                         $('#temperature'+[i]).text((((data.daily[i].temp.max-273)*1.8)+32).toFixed(2));
                                         $('#wind'+[i]).text(data.daily[i].wind_speed);
                                         $('#humidity'+[i]).text(data.daily[i].humidity);
                                         $('#cityDate'+[i]).text(moment().add([i]*1, 'days').format('MM/DD/YYYY'));
                                         $('#wicon'+[i]).attr('src', iconURL);
                                     }
-                                    console.log(data.daily);
-                                    console.log('fivedays of temps', fiveDay);
                                     dailyWeather();                                    
                                     previousSearch();
                                 })
@@ -65,7 +54,64 @@ searchBtn[0].addEventListener('click', function(){
             }
         })
 });
-
+for (let k = 0; k < historyBtn.length; k++) {   
+    $('#searchBtn'+k).addEventListener('click', function(){
+        searchHisotry = JSON.parse(localStorage.getItem('citySearch'));
+        city = searchHisotry[k].value.trim()
+        console.log(city);
+        var testURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial'+apiKey
+        var temp2 = fetch(testURL)
+        .then(function (response){
+            if (response.ok) {
+                response.json().then(function (data2){                    
+                    console.log(data2);
+                    lat = data2.coord.lat
+                    console.log(lat)
+                    lon = data2.coord.lon
+                    console.log(lon);
+                    city = data2.name
+                    storeSearch();
+                    var latlonURL = 'http://api.openweathermap.org/data/2.5/onecall?lat=-'+lat+'&lon='+lon+'&exclude=minutely,hourly&appid=f516944114208483a59c1cf67915c8cd'
+                    var temp = fetch(latlonURL)
+                    .then(function (response){
+                        if (response.ok) {
+                            response.json().then(function (data){
+                                console.log(`temp`, temp);
+                                console.log(`data`, data);
+                                windSpeed = data.current.wind_speed + ' ';
+                                humidity = data.current.humidity + ' ';
+                                uvIndex = data.current.uvi;
+                                tempK = data.current.temp;
+                                tempC = tempK - 273;
+                                tempF = (tempC*1.8) + 32;
+                                console.log('tempC', tempC);                    
+                                date = moment().format('MM/DD/YYYY');
+                                iconCurrent = data.current.weather[0].icon
+                                var fiveDay = [];
+                                for (let i = 1; i < 6; i++) {
+                                    icon = data.daily[i].weather[0].icon;
+                                    var iconURL = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
+                                    console.log('chosen icon',icon);
+                                    $('#temperature'+[i]).text((((data.daily[i].temp.max-273)*1.8)+32).toFixed(2));
+                                    $('#wind'+[i]).text(data.daily[i].wind_speed);
+                                    $('#humidity'+[i]).text(data.daily[i].humidity);
+                                    $('#cityDate'+[i]).text(moment().add([i]*1, 'days').format('MM/DD/YYYY'));
+                                    $('#wicon'+[i]).attr('src', iconURL);
+                                }
+                                console.log(data.daily);
+                                console.log('fivedays of temps', fiveDay);
+                                dailyWeather();                                    
+                                previousSearch();
+                            })
+                            
+                        }
+                    })
+                })
+            }
+        })
+    });
+}
+    
 
 function dailyWeather() {
     var currentIconURL = 'http://openweathermap.org/img/wn/'+iconCurrent+'@2x.png'
@@ -98,11 +144,11 @@ function storeSearch() {
 function previousSearch() {
     
     searchHisotry = JSON.parse(localStorage.getItem('citySearch'));
-    $('button').remove('.searchbtn');
+    $('button').remove('.historyBtn');
     for (let i = 0; i < searchHisotry.length; i++) {
         $('#searchHistory').after($('<button/>', {
             text: searchHisotry[i].city,
-            class: 'btn btn-primary d-flex flex-column searchbtn',
+            class: 'btn btn-primary d-flex flex-column historyBtn',
             id: 'searchbtn'+i,
         }))
         
